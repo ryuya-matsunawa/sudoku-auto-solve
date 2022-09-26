48241b0c8a68e5105e3c78d2e5ecd6c1f5ae1e58
ryuya-matsunawa <ruymtnw@gmail.com>
2022-09-26 11:00:40 +0900

    feat: kivy-ios

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
 from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
 
-App().run()
+from kivy.lang import Builder
+Builder.load_file('./SampleGrid.kv')
+
+class MyGrid1(GridLayout):
+    pass
+
+class SampleGridLayoutApp(App):
+    def build(self):
+        root = MyGrid1()
+        return root
+
+if __name__ == '__main__':
+    SampleGridLayoutApp().run()
