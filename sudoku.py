6a7d655197938b04d5c6169cc575da5d84c34515
ryuya-matsunawa <ruymtnw@gmail.com>
2022-10-03 00:57:58 +0900

    fix: logic

--- sudoku.py
+++ sudoku.py
@@ -2,20 +2,15 @@
 from itertools import product
 from select import select
 from kivy.uix.button import Button
-from kivy.graphics import Color, Rectangle
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.behaviors import FocusBehavior
 import solve
 
-class MainGrid(FocusBehavior, GridLayout):
+class MainGrid(GridLayout):
     pass
-    def __init__(self, **kwargs):
-        super(MainGrid, self).__init__(**kwargs)
 
 class SubGrid(GridLayout):
-    def __init__(self, **kwargs):
-        super(SubGrid, self).__init__(**kwargs)
+    pass
 
 class Cell(Button):
     DIC = {0: "*", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"}
@@ -25,9 +20,6 @@ class Cell(Button):
         self.text = Cell.DIC[0]
 
 class SudokuApp(App):
-    def __init__(self, **kwargs):
-        super(SudokuApp, self).__init__(**kwargs)
-
     def build(self):
         self.cells = dict()
         main_grid = self.root.ids.main_grid
@@ -43,32 +35,30 @@ class SudokuApp(App):
         number_grid = self.root.ids.number_grid
         for index in range(1, 10):
             cell = Cell()
-            cell.fbind("on_press", self.change_cell)
+            cell.fbind("on_press", self.set_number)
             cell.text = Cell.DIC[index]
             number_grid.add_widget(cell)
         # return main_grid
 
-    def select_cell(self, cell, pos):
+    def select_cell(self, instance, pos):
         if self.selected_cell is not None:
-            self.selected_cell.canvas.before.clear()
-        self.selected_cell = cell
+            self.selected_cell.background_color = (1, 1, 1, 1)
+        self.selected_cell = instance
         self.selected_cell_pos = pos
-        with cell.canvas.before:
-            Color(1, 1, 0, 2)
-            Rectangle(pos=cell.pos, size=cell.size)
+        self.selected_cell.background_color = (1, 1, 0, 1)
 
-    def change_cell(self, cell):
-        self.cells[self.selected_cell_pos].text = cell.text
+    def set_number(self, instance):
+        if self.selected_cell:
+            self.cells[self.selected_cell_pos].text = instance.text
 
     def solve(self):
         problem = [[0] * 9 for _ in range(9)]
-        for (i, j) in product(range(3), repeat=2):
+        for (i, j) in product(range(9), repeat=2):
             cell = self.cells[(i, j)].text
             problem[i][j] = int(cell) if cell != "*" else 0
         answers = solve.main(problem)
         for (i, j) in product(range(9), repeat=2):
             self.cells[(i, j)].text = Cell.DIC[answers[0][i][j]]
-        pass
 
     def reset(self):
         solve.answers = []
